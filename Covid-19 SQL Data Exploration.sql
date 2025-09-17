40a2e93d8344d6fe07b1acb005ac3594f3b20503
Ranim-A7 <ranim.amin1@gmail.com>
2025-09-17 10:35:53 -0500

    Covid-19 SQL Data Exploration and Dashboard.sql

--- Covid-19 SQL Data Exploration.sql
+++ Covid-19 SQL Data Exploration.sql
@@ -148,3 +148,7 @@ From PortfolioProject..CovidDeaths$
 -- location like'%states%'
 Group by Location, population, date
 order by PercentofPopulationInected desc
+
+Link to Tableau Dashboard
+https://public.tableau.com/views/Covid-19DataDashboard_17351918934050/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link
+
